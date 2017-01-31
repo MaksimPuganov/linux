8ded192883f2a3926f8591d8d13f03ebceae1463
Jason Pell <jason@pellcorp.com>
2017-02-01 08:47:16 +1100

    cleanup old scripts

--- bin/dualmon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xrandr --output VGA1 --primary
-xrandr --auto --output VGA1 --left-of LVDS1
-
--- bin/windows.sh
+++
@@ -1,2 +0,0 @@
-xfreerdp --no-nla -d DOMAIN -u USER -p PASSWORD --plugin cliprdr --gdi hw -g 1300x720 -x 100 -x 80 HOSTIP &
-
