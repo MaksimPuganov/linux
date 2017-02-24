d101a62afaf2fc01b0181eff4f29f858c9bc623c
Jason Pell <jason@pellcorp.com>
2017-02-24 22:14:03 +1100

    wait for ip address to be assigned in case vmware is a bit slow

--- aqi-wordpress/startup.sh
+++ aqi-wordpress/startup.sh
@@ -2,16 +2,25 @@
 
 ETH_DEVICE=$(ip link | grep ": e" | tr ':' ' ' | awk '{print $2}')
 
-IP_ADDRESS=$(ip addr show dev $ETH_DEVICE | sed -nr "s/.*inet ([^/]+).*/\1/p")
+echo ""
+
+while true; do
+	IP_ADDRESS=$(ip addr show dev $ETH_DEVICE | sed -nr "s/.*inet ([^/]+).*/\1/p")
+	if [ "$IP_ADDRESS" != "" ]; then
+		break;
+	else
+		echo "Waiting for IP Address..."
+		sleep 10
+	fi
+done
 
 echo ""
 echo "Your current IP Address is $IP_ADDRESS"
 echo ""
 
 SCRIPT="update wp_options set option_value = 'http://$IP_ADDRESS' where option_name in ('siteurl', 'home')"
-echo $SCRIPT | mysql -u root --password=password aqidb 2>/dev/null
+echo $SCRIPT | mysql -u root --password=password  aqidb  2>/dev/null
 
 # ensure that wordpress uses the updated config, need to restart apache2
-# TODO - only do this if the IP has changed.
 sudo systemctl restart apache2
 
