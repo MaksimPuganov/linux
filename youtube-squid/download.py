5a8aa1154ece8d1bc81513d59ffa1b4431a08d08
Jason Pell <jason@pellcorp.com>
2017-10-24 21:32:26 +1100

    converted to tabs

--- youtube-squid/download.py
+++ youtube-squid/download.py
@@ -16,44 +16,43 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 def response(r):
-    sys.stdout.write("%s\n" % r)
-    sys.stdout.flush()
+	sys.stdout.write("%s\n" % r)
+	sys.stdout.flush()
 
 def download(url):
-    try:
-        #UCu6mSoMNzHQiBIOCkHUa2Aw
-        #httplib2.debuglevel=4
-        h = httplib2.Http(proxy_info = httplib2.ProxyInfo(socks.PROXY_TYPE_HTTP, 'localhost', 3128), disable_ssl_certificate_validation=True)
+	try:
+		#UCu6mSoMNzHQiBIOCkHUa2Aw
+		#httplib2.debuglevel=4
+		h = httplib2.Http(proxy_info = httplib2.ProxyInfo(socks.PROXY_TYPE_HTTP, 'localhost', 3128), disable_ssl_certificate_validation=True)
 
-        logger.info(url)
-        r, content = h.request(url, "GET")
+		logger.info(url)
+		r, content = h.request(url, "GET")
 
-        match = re.compile('"ucid":"([^"]+)"').findall(content)
-        if match:
-            return match[0]
-        else:
-            logger.error("Could not find ucid")
-            return ""
-    except:
-        logger.error("Failed to download url")
-        return ""
+		match = re.compile('"ucid":"([^"]+)"').findall(content)
+		if match:
+			return match[0]
+		else:
+			logger.error("Could not find ucid")
+			return ""
+	except:
+		logger.error("Failed to download url")
+		return ""
 
 while True:
-    try:
-        line = sys.stdin.readline().strip()
+	try:
+		line = sys.stdin.readline().strip()
 
-        if line == "":
-            exit()
-        elif line.startswith("https://www.youtube.com/watch"):
-            ucid = download(line)
-            if ucid == 'UCu6mSoMNzHQiBIOCkHUa2Aw':
-                response("OK")
-            else:
-                response("ERR message=Invalid%20Channel")
-        elif line.startswith("https://www.youtube.com"):
-            response("OK")
-	else:
-	    response("ERR")
-    except:
-        pass
+		if line == "":
+			exit()
+		elif line.startswith("https://www.youtube.com/embed"):
+			logger.info(line)
+			ucid = download(line)
+			if ucid == 'UCu6mSoMNzHQiBIOCkHUa2Aw':
+				response("OK")
+			else:
+				response("ERR message=Invalid%20Channel")
+		else:
+			response("ERR")
+	except:
+		pass
 
